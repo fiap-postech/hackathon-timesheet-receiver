79e8338f61115fa5ce9a3ae368dc13f0095c4422
giovanischram <schram.giovani@gmail.com>
2024-03-22 19:42:58 -0300

    first commit

---
+++ app/app.mjs
@@ -0,0 +1,40 @@
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
+
+const sqsClient = new SQSClient({ region: process.env.AWS_LAMBDA_REGION });
+
+export const handler = async (event) => {
+    try {
+        console.log(`Event: ${JSON.stringify(event)}`);
+
+        const requestBody = JSON.parse(event.body);
+        const employeeId = event.headers['x-employee-id'];
+        const employeeEmail = event.headers['x-employee-email'];
+
+        const messageBody = {
+            ...requestBody,
+            "employeeId": employeeId,
+            "employeeEmail": employeeEmail
+        };
+
+        const messageBodyAsString = JSON.stringify(messageBody);
+        console.log(`messageBody: ${messageBodyAsString}`);
+
+        const sqsResponse = await sqsClient.send(new SendMessageCommand({
+            QueueUrl: process.env.AWS_SQS_QUEUE_URL,
+            MessageBody: messageBodyAsString
+        }));
+
+        console.log(`Message sent to SQS: ${JSON.stringify(sqsResponse)}`);
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify("Message sent to SQS"),
+        };
+    } catch (err) {
+        console.error(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify("Error occurred"),
+        };
+    }
+};
